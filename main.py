60a24b658d92720e3af853d58beda0467a7a492e
Gabriel Xavier <146781954+xafer-gab@users.noreply.github.com>
2026-01-14 18:50:14 -0300

    Atualização de "LEIA" - Leitura Musical Interativa e Algorítmica
    Atualização da aplicação "LEIA", incluindo novas funções, melhora do processamento dos dados do usuário e resolução de bugs.
    
    1. Resolução de bugs com probabilidade de pausas.
    2. Gestão de arquivos temporários.
    3. Adição da funcionalidade de "dispersão melódica".
    4. Diagramação (Gradio) atualizada com disposição instrutiva.
    5. Inclusão de reprodutor de áudio a partir de Fluidsynth.
    6. Mudança de inúmeras funções de geração, formatação e renderização.

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
-from src import gera, formata, renderiza
+import os
+from src import gera, formata, renderiza, mid_aud
 from data import duracoes, escalas
 from gui.interface import interface
 
-#Variáves globais (mudar a forma de selecionar)
-fundamental = "Dó"
-escala = "Maior" 
-modelo = "Jônio"
-clave = "Sol"
-oitavas = 1 #[1, 2, 3]
-form_comp = "4/4"
-fig_selec = ["Colcheia", "Semicolcheia"]
-ligadura = False
-pausa_p = 0
-num_comp = 12  
-diretorio = "/tmp"
+#Produz diretório de arquivos temporários
+pasta_temp = "tmp_part"
+def prepara_tmp():
+    #Cria a pasta se não existir
+    if not os.path.exists(pasta_temp):
+        os.makedirs(pasta_temp)
 
-def gera_formata_renderiza(fundamental, escala, modelo, clave, oitavas, form_comp, fig_selec, ligadura, pausa_p, num_comp, diretorio):
+    #Limpa arquivos temporários
+    for arq in os.listdir(pasta_temp):
+        arquivo = os.path.join(pasta_temp, arq)
+        os.remove(arquivo)
+
+def gera_formata_renderiza(
+        fundamental, escala, modelo, clave, oitavas, 
+        num_comp, form_comp, pausa_p, dispersao, ligadura,
+        fig_selec,
+        andamento_midi, inst_midi):
+    
+    #Gere arquivos temporários
+    prepara_tmp()
+    
+    #Durações
+    diretorio = "tmp_part"
     lis_dur_selec = gera.gera_lista_dur(fig_selec)
     f_c = duracoes.form_compasso[form_comp]
     duracoes_rand = gera.adiciona_valores(lis_dur_selec, f_c[0], f_c[1], num_comp, ligadura=ligadura)
-    alturas_rand = gera.nota_randomica(fundamental, escala, modelo, len(duracoes_rand), clave, oitavas, pausa_probab=pausa_p)
+    
+    #Alturas
+    if not clave == "Percussão":
+        alturas_rand = gera.nota_randomica(
+            fundamental, escala, modelo, len(duracoes_rand), 
+            clave, oitavas, pausa_probab=pausa_p, dispersao=(dispersao/100)
+            )
+    else:
+        alturas_rand = gera.nota_percussao(len(duracoes_rand), pausa_probab=pausa_p)
+    
+    #Formatação
     notas = formata.dur_formata(duracoes_rand, alturas_rand, f_c[0], f_c[1])
-    l_script = renderiza.comp_lily(notas, clave, fundamental, escala, form_comp)
-    exporta = renderiza.exporta(l_script, diretorio, formato="png")
-    return exporta
+    l_script = renderiza.comp_lily(notas, clave, fundamental, escala, form_comp, andamento_midi, inst_midi)
+    partitura, audio_dir = renderiza.exporta(l_script, diretorio, formato="png")
+    
+    #Gera arquivo de áudio
+    mid_aud.midi_para_wav(f"{audio_dir}.midi", f"{audio_dir}.wav")
+    
+    #Retorna partitura (.png) e áudio (.wav)
+    return partitura, f"{audio_dir}.wav"
 
 interface(gera_formata_renderiza)
